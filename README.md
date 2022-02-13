40b250fd601bafe5335a1851cc5bd9209fd91501
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2022-02-13 12:43:38 -0600

    DEAL WITH IT

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 Think more laterally.
 
-Slack bot that returns a randomly-selected [Oblique Strategy](http://www.rtqe.net/ObliqueStrategies/Ed1.html) along with a link to a random [I Ching](http://www.akirarabelais.com/i/i.html) hexagram. Randomness courtesy of [random.org](http://random.org), for which you will want to get an [API key](https://api.random.org/api-keys).
-
-Now also returns a random Strategy to the web as text at /strategy ([for example](http://oblique.ty-m.xyz/strategy)) 
-
-And a random hexagram via [/i_ching](http://oblique.ty-m.xyz/i_ching).
+CLI script that returns a randomly-selected [Oblique Strategy](http://www.rtqe.net/ObliqueStrategies/Ed1.html). Randomness courtesy of [random.org](http://random.org), for which you will want to get an [API key](https://api.random.org/api-keys).
--- oblique.py
+++ oblique.py
@@ -1,26 +1,20 @@
-from flask import Flask
-from flask_slack import Slack
-from rdoclient import RandomOrgClient as rorgcli
+# from flask import Flask
+# from flask_slack import Slack
 import os
 
-app = Flask(__name__)
-slack = Slack(app)
-app.add_url_rule('/', view_func=slack.dispatch)
-
-slacktoken = os.getenv('SLACKTOKEN')
-randokey = os.getenv('RANDOMORG_KEY')
-team = os.getenv('SLACKTEAM')
+from rdoclient import RandomOrgClient as rorgcli
 
 
 def random_client():
-    rnd = rorgcli(randokey)
+    rnd = rorgcli(os.getenv("RANDOMORG_KEY"))
     return rnd
+
 
 rnd = random_client()
 
 
 def strategy():
-    with open('oblique.txt', 'r') as ost:
+    with open("oblique.txt", "r") as ost:
         strats = ost.readlines()
         length = len(strats)
     idx = rnd.generate_integers(1, 0, length)
@@ -28,46 +22,6 @@ def strategy():
     return strat
 
 
-def i_ching():
-    with open('iching-title.txt', 'r') as i:
-        chis = i.readlines()
-    idx = rnd.generate_integers(1, 0, 63)
-    hexagram = chis[idx[0]].strip()
-    hexagram_no = idx[0] + 1
-    url = 'http://www.akirarabelais.com/i/i.html#%s' % hexagram_no
-    return {'hexagram': hexagram, 'url': url}
-
-
-@slack.command('oblique',
-               token=slacktoken,
-               team_id=team,
-               methods=['POST'])
-def oblique(**kwargs):
+if __name__ == "__main__":
     strat = strategy()
-    iching = i_ching()
-    url = iching['url']
-    hexagram = iching['hexagram']
-    message = '%s\n <%s|%s>' % (strat, url, hexagram)
-    return message
-    return slack.response(message)
-
-
-# strategy text url
-@app.route('/strategy')
-def strategy_txt():
-    strat = strategy() + '\n'
-    return strat
-
-
-# iching text url
-@app.route('/i_ching')
-def i_ching_txt():
-    iching = i_ching()
-    hexagram = iching['hexagram']
-    url = url = iching['url']
-    i_ching_md = '[%s](%s)' % (hexagram, url)
-    return i_ching_md
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    print(strat)
